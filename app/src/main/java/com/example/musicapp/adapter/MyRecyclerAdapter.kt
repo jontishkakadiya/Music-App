7e3193b43f24b8d1d7fc82cc33f7a0624505b00d
“jontish <jontishkakadiya@gmail.com>
2024-05-03 22:37:35 -0400

    New Songs playing screen

--- app/src/main/java/com/example/musicapp/adapter/MyRecyclerAdapter.kt
+++ app/src/main/java/com/example/musicapp/adapter/MyRecyclerAdapter.kt
@@ -48,6 +48,49 @@ class MyRecyclerAdapter(private val dataList: List<Data>) :
         }
     }
 }
-
-
-
+//
+//
+//package com.example.musicapp.adapter
+//
+//import android.app.Activity
+//import android.content.Intent
+//import android.media.MediaPlayer
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import androidx.core.net.toUri
+//import androidx.recyclerview.widget.RecyclerView
+//import com.example.musicapp.MusicPlayActivity
+//import com.example.musicapp.data.Data
+//import com.example.musicapp.databinding.RecyclerviewRowBinding
+//import com.squareup.picasso.Picasso
+//
+//class MyRecyclerAdapter(private val context: Activity, private val dataList: List<Data>) :
+//    RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder>() {
+//
+//    class MyViewHolder(private val binding: RecyclerviewRowBinding) :
+//        RecyclerView.ViewHolder(binding.root) {
+//
+//        fun bind(data: Data, mediaPlayer: MediaPlayer, context: Activity) {
+//            binding.apply {
+//                musicTitle.text = data.title
+//                artistName.text = data.artist.name
+//                Picasso.get().load(data.album.cover).into(musicImage)
+//
+//            }
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//        val inflater = LayoutInflater.from(parent.context)
+//        val binding = RecyclerviewRowBinding.inflate(inflater, parent, false)
+//        return MyViewHolder(binding)
+//    }
+//
+//    override fun getItemCount(): Int = dataList.size
+//
+//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//        val currentData = dataList[position]
+//        val mediaPlayer = MediaPlayer.create(context, currentData.preview?.toUri())
+//        holder.bind(currentData, mediaPlayer, context)
+//    }
+//}
